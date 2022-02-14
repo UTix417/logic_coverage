a4c64b2e52fa0620ab0d50fa766617eae88074ef
lxf <lxf417@qq.com>
2022-02-14 23:07:27 +0800

    20220214

--- src/App.js
+++ src/App.js
@@ -1,17 +1,64 @@
 import './App.css';
+import React from "react";
 import { Input, Button, Typography } from 'antd';
-import { FundFilled, SettingFilled } from '@ant-design/icons';
-
-
+import { PaperClipOutlined, BarChartOutlined } from '@ant-design/icons';
+const { TextArea } = Input;
 const { Title } = Typography;
-function App() {
-  return (
-    <div className="App">
-      <Title><FundFilled /></Title>
-      <Input placeholder="Basic usage"/>
-      <Button type="primary" shape="circle" icon={<SettingFilled />} />
-    </div>
-  );
+
+class App extends React.Component{
+
+  constructor(){
+    super();
+    this.state = {
+      text: "",
+      loading: false,
+    }
+  }
+
+  work = () => {
+    this.setState({
+      loading: true,
+    },() => {
+      this.setState({
+        loading: false,
+      });
+    });
+  }
+
+  input = e => {
+    this.setState({
+      text: e.target.value,
+    });
+  }
+
+  render(){
+    return (
+      <div>
+        <div className="App">
+          <Title className="title">
+            <BarChartOutlined />
+            <span style={{fontSize:'30px',marginLeft:'10px'}}>布尔表达式的逻辑覆盖</span>
+          </Title>
+          <div className="input-box">
+            <TextArea className="input" 
+              value={this.state.text} 
+              autoSize={{minRows: 3, maxRows: 3}}
+              onPressEnter={this.work.bind()} 
+              onChange={this.input.bind()}
+              placeholder="Basic usage"
+            />
+            <Button className="button"
+              type="primary" 
+              icon={<PaperClipOutlined />}
+              onClick={this.work.bind()}
+              loading={this.state.loading}
+            />  
+          </div>
+        </div>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
