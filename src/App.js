7aca3a547b902e8a2164b41e28a2fefd3248950c
lxf <lxf417@qq.com>
2022-02-27 21:47:39 +0800

    20220227

--- src/App.js
+++ src/App.js
@@ -27,7 +27,8 @@ class App extends React.Component{
     let temp = this.state.text;
     let tempLen = temp.length;
     let tempArray = [];//用来记录括号
-    let flag=false;
+    let kuohaoflag=false;
+    let sysflag=false;
     temp = temp.replace(/\s/g,"");
     for(let i=0;i<tempLen;i++){
       if(temp[i]==='('){
@@ -35,13 +36,33 @@ class App extends React.Component{
       }
       if(temp[i]===')'){
         if(tempArray.length===0){
-          flag=true;
+          kuohaoflag=true;
           break;
         }
         tempArray.pop();
       }
+      if(i>0){
+        if(temp[i]==temp[i-1]&&(temp[i]=='&'||temp[i]=='|')){
+          sysflag=true;
+          break;
+        }
+      }
     }
-    if(tempArray.length>0||flag){
+    if(sysflag){
+      this.setState({
+        loading: false,
+      },()=>{
+        notification['error']({
+          message: '您输入的表达式有算符错误，请检查',
+          description: null,
+          onClick: () => {
+            
+          },
+        });
+      });
+      return;
+    }
+    if(tempArray.length>0||kuohaoflag){
       this.setState({
         loading: false,
       },()=>{
@@ -59,7 +80,231 @@ class App extends React.Component{
   }
 
   divi = (str) =>{
+    let front=0;
+    let back=0;
+    for(let i=0;i<str.length;i++){
+      if(str[i]=='('){
+        break;
+      }
+      front++;
+    }
+    for(let i=str.length-1;i>=0;i--){
+      if(str[i]==')'){
+        break;
+      }
+      back++;
+    }
+    let num=Math.min(front,back);
+    let tureStr=str.substring(num,str.length-num)
+    for(let i=0;i<tureStr.length;i++){
+      if(tureStr[i]=='('){
+        return;
+        this.makeElementSimpleWithNo(tureStr);
+      }
+    }
+    this.workWithHave(tureStr);
+  }
 
+  workWithNo = (syn,pos) => {
+    
+  }
+
+  workWithHave = (tureStr) => {
+    
+  }
+
+  makeElementSimpleWithNo = (tureStr) => {
+    let syn={};  
+    let pos={};
+    for(let i=0;i<tureStr.length;){
+      let eleName='';
+      let eleSyn='';
+      let lasts=[];//用来维护算符段终点
+      let notCount=0;
+      if(tureStr[i]=='|'||tureStr=='&'){
+        if(eleSyn!=tureStr[i]){
+          lasts.push(i);
+        }
+        eleSyn=tureStr[i];
+      }
+      while(true){
+        if(trueStr[i]=='!'){
+          notCount++;
+          if(tureStr=='|'||tureStr=='&'||i==tureStr.length){
+            break;
+          }else if(eleName.length){
+            this.setState({
+              loading: false,
+            },()=>{
+              notification['error']({
+                message: '您输入的表达式有算符错误，请检查',
+                description: null,
+                onClick: () => {
+                  
+                },
+              });
+            });
+            return;
+          }
+        }else{
+          eleName+=tureStr[i];
+        }
+        i++;
+      }
+      if(notCount&1){
+        eleSyn+='!';
+      } 
+      if(syn[eleName]){
+        if(syn[eleName]==eleSyn){
+
+        }else{
+          if(syn[eleName]=='|'){
+            if(eleSyn=='&!'){
+              syn[eleName]=eleSyn;
+              pos[eleName]=i;
+            }
+            if(eleSyn=='|!'){
+              syn[eleName]=eleSyn;
+              pos[eleName]=i;
+            }
+            if(eleSyn=='&'){
+              let last=0;
+              for(let j=0;j<lasts.length;j++){
+                if(lasts[j]>pos[eleName]){
+                  last=lasts[j];
+                }
+              }
+              Object.keys(obj).forEach(()=>{
+                if(pos[obj]<last){
+                  delete pos[obj];
+                  delete syn[obj];
+                }
+              })
+              syn[eleName]=eleSyn;
+              pos[eleName]=i;
+            }
+            if(eleSyn=='|'){
+              pos[eleName]=i;
+            }
+          }
+          if(syn[eleName]=='|!'){
+            if(eleSyn=='&'){
+              syn[eleName]=eleSyn;
+              pos[eleName]=i;
+            }
+            if(eleSyn=='|'){
+              syn[eleName]=eleSyn;
+              pos[eleName]=i;
+            }
+            if(eleSyn=='&!'){
+              let last=0;
+              for(let j=0;j<lasts.length;j++){
+                if(lasts[j]>pos[eleName]){
+                  last=lasts[j];
+                }
+              }
+              Object.keys(obj).forEach(()=>{
+                if(pos[obj]<last){
+                  delete pos[obj];
+                  delete syn[obj];
+                }
+              })
+              syn[eleName]=eleSyn;
+              pos[eleName]=i;
+            }
+            if(eleSyn=='|!'){
+              pos[eleName]=i;
+            }
+          }
+          if(syn[eleName]=='&'){
+            if(eleSyn=='&!'){
+              let last=0;
+              for(let j=0;j<lasts.length;j++){
+                if(lasts[j]>pos[eleName]){
+                  last=lasts[j];
+                }
+              }
+              Object.keys(obj).forEach(()=>{
+                if(pos[obj]<last){
+                  delete pos[obj];
+                  delete syn[obj];
+                }
+              })
+              syn[eleName]=eleSyn;
+              pos[eleName]=i;
+            }
+            if(eleSyn=='|!'){
+              syn[eleName]=eleSyn;
+              pos[eleName]=i;
+            }
+            if(eleSyn=='&'){
+              pos[eleName]=i;
+            }
+            if(eleSyn=='|'){
+              let last=0;
+              for(let j=0;j<lasts.length;j++){
+                if(lasts[j]>pos[eleName]){
+                  last=lasts[j];
+                }
+              }
+              Object.keys(obj).forEach(()=>{
+                if(pos[obj]<last){
+                  delete pos[obj];
+                  delete syn[obj];
+                }
+              })
+              syn[eleName]=eleSyn;
+              pos[eleName]=i;
+            }
+          }
+          if(syn[eleName]=='&!'){
+            if(eleSyn=='&'){
+              let last=0;
+              for(let j=0;j<lasts.length;j++){
+                if(lasts[j]>pos[eleName]){
+                  last=lasts[j];
+                }
+              }
+              Object.keys(obj).forEach(()=>{
+                if(pos[obj]<last){
+                  delete pos[obj];
+                  delete syn[obj];
+                }
+              })
+              syn[eleName]=eleSyn;
+              pos[eleName]=i;
+            }
+            if(eleSyn=='|'){
+              syn[eleName]=eleSyn;
+              pos[eleName]=i;
+            }
+            if(eleSyn=='&!'){
+              pos[eleName]=i;
+            }
+            if(eleSyn=='|!'){
+              let last=0;
+              for(let j=0;j<lasts.length;j++){
+                if(lasts[j]>pos[eleName]){
+                  last=lasts[j];
+                }
+              }
+              Object.keys(obj).forEach(()=>{
+                if(pos[obj]<last){
+                  delete pos[obj];
+                  delete syn[obj];
+                }
+              })
+              syn[eleName]=eleSyn;
+              pos[eleName]=i;
+            }
+          }
+        }
+      }else{
+        syn[eleName]=eleSyn;
+        pos[eleName]=i;
+      }
+    }  
+    workWithNo(syn,pos)
   }
 
   input = e => {
