c8501803247314419ea61274178020305e335750
lxf <lxf417@qq.com>
2022-03-02 22:50:37 +0800

    20220302

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ class App extends React.Component{
         tempArray.pop();
       }
       if(i>0){
-        if(temp[i]==temp[i-1]&&(temp[i]=='&'||temp[i]=='|')){
+        if(temp[i]===temp[i-1]&&(temp[i]==='&'||temp[i]==='|')){
           sysflag=true;
           break;
         }
@@ -83,13 +83,13 @@ class App extends React.Component{
     let front=0;
     let back=0;
     for(let i=0;i<str.length;i++){
-      if(str[i]=='('){
+      if(str[i]==='('){
         break;
       }
       front++;
     }
     for(let i=str.length-1;i>=0;i--){
-      if(str[i]==')'){
+      if(str[i]===')'){
         break;
       }
       back++;
@@ -97,7 +97,7 @@ class App extends React.Component{
     let num=Math.min(front,back);
     let tureStr=str.substring(num,str.length-num)
     for(let i=0;i<tureStr.length;i++){
-      if(tureStr[i]=='('){
+      if(tureStr[i]==='('){
         return;
         this.makeElementSimpleWithNo(tureStr);
       }
@@ -118,6 +118,93 @@ class App extends React.Component{
     
   }
 
+  toFormat = async (str) => {
+    let startIndex=-1;
+    let num=[];
+    let nowNotFlag=0;
+    //去括号递归
+    for(let i=0;i<str.length;i++){
+        if(str[i]=='('&&num.length==0){
+            startIndex=i+1;
+            num.push(i);
+            if(i>0){
+                if(str[i]=='!'){
+                    nowNotFlag=1;
+                }
+            }
+        }
+        if(nowNotFlag){
+            if(str[i]=='&'){
+                str=str.subString(0,i-1)+'|'+str.subString(i+1,str.length);
+            }else if(str[i]=='|'){
+                str=str.subString(0,i-1)+'&'+str.subString(i+1,str.length);
+            }else if(str[i]=='!'){
+                let j=i+1;
+                while(j<str.length&&(str[j]!='|'||str[j]!='&')){
+                    j++;
+                }
+                str=str.subString(0,i-1)+str.subString(i+1,str.length);
+                i=j-1;
+            }else if(str[i]=='('){
+                let j=i+1;
+                while(j<str.length&&str[j]!=')'){
+                    j++;
+                }
+                str=str.subString(0,i-1)+'!'+str.subString(i,str.length);
+                i=j;
+            }else{
+                let j=i+1;
+                while(j<str.length&&(str[j]!='|'||str[j]!='&')){
+                    j++;
+                }
+                str=str.subString(0,i-1)+'!'+str.subString(i,str.length);
+                i=j-1;
+            }
+        }
+        if(str[i]==')'){
+            num.pop();
+            if(num.length==0){
+                let res=await this.toFormat(str.subString(startIndex,i));
+                let tempStr=str;
+                str='['+res+']';
+                if(startIndex>0){
+                    str=tempStr.subString(0,startIndex-1-nowNotFlag)+str;
+                }
+                i=str.length-1;
+                if(i<tempStr.length-1){
+                    str=str+tempStr.subString(i+1,tempStr.length);
+                }
+            }
+        }
+    }
+    //括号内运算
+    let lastSyn='';
+    let deleteStart=-1;
+    let andFlag=false;
+    for(let i=0;i<str.length;i++){
+        if(str[i]=='|'||str[i]=='&'){
+            lastSyn=str[i];
+        }
+        if(str[i]=='['){
+            deleteStart=i+1;
+            if(lastSyn=='|'||lastSyn==''){
+                
+            }else{
+                andFlag=true;
+            }
+        }
+        if(str[i]==']'){
+            deleteStart=-1;
+            if(!andFlag){
+                str=str.subString(0,deleteStart-2)+str.subString(deleteStart,i-1);
+            }else{
+
+                andFlag=false;
+            }
+        }
+    }
+  }
+
   makeElementSimpleWithNo = (tureStr) => {
     let syn={};  
     let pos={};
@@ -126,16 +213,16 @@ class App extends React.Component{
       let eleSyn='';
       let lasts=[];//用来维护算符段终点
       let notCount=0;
-      if(tureStr[i]=='|'||tureStr=='&'){
+      if(tureStr[i]==='|'||tureStr==='&'){
         if(eleSyn!=tureStr[i]){
           lasts.push(i);
         }
         eleSyn=tureStr[i];
       }
       while(true){
-        if(trueStr[i]=='!'){
+        if(tureStr[i]==='!'){
           notCount++;
-          if(tureStr=='|'||tureStr=='&'||i==tureStr.length){
+          if(tureStr==='|'||tureStr==='&'||i===tureStr.length){
             break;
           }else if(eleName.length){
             this.setState({
@@ -160,19 +247,19 @@ class App extends React.Component{
         eleSyn+='!';
       } 
       if(syn[eleName]){
-        if(syn[eleName]==eleSyn){
+        if(syn[eleName]===eleSyn){
 
         }else{
-          if(syn[eleName]=='|' || syn[eleName]==''){
-            if(eleSyn=='&!'){
+          if(syn[eleName]==='|' || syn[eleName]===''){
+            if(eleSyn==='&!'){
               syn[eleName]=eleSyn;
               pos[eleName]=i;
             }
-            if(eleSyn=='|!'){
+            if(eleSyn==='|!'){
               syn[eleName]=eleSyn;
               pos[eleName]=i;
             }
-            if(eleSyn=='&'){
+            if(eleSyn==='&'){
               let last=0;
               for(let j=0;j<lasts.length;j++){
                 if(lasts[j]>pos[eleName]){
@@ -181,122 +268,122 @@ class App extends React.Component{
               }
               Object.keys(pos).forEach((key)=>{
                 if(pos[key]<last){
-                  delete pos[obj];
-                  delete syn[obj];
+                  delete pos[key];
+                  delete syn[key];
                 }
               })
               syn[eleName]=eleSyn;
               pos[eleName]=i;
             }
-            if(eleSyn=='|'){
+            if(eleSyn==='|'){
               pos[eleName]=i;
             }
           }
-          if(syn[eleName]=='|!' || syn[eleName]=='!'){
-            if(eleSyn=='&'){
+          if(syn[eleName]==='|!' || syn[eleName]==='!'){
+            if(eleSyn==='&'){
               syn[eleName]=eleSyn;
               pos[eleName]=i;
             }
-            if(eleSyn=='|'){
+            if(eleSyn==='|'){
               syn[eleName]=eleSyn;
               pos[eleName]=i;
             }
-            if(eleSyn=='&!'){
+            if(eleSyn==='&!'){
               let last=0;
               for(let j=0;j<lasts.length;j++){
                 if(lasts[j]>pos[eleName]){
                   last=lasts[j];
                 }
               }
-              Object.keys(obj).forEach(()=>{
-                if(pos[obj]<last){
-                  delete pos[obj];
-                  delete syn[obj];
+              Object.keys(pos).forEach((key)=>{
+                if(pos[key]<last){
+                  delete pos[key];
+                  delete syn[key];
                 }
               })
               syn[eleName]=eleSyn;
               pos[eleName]=i;
             }
-            if(eleSyn=='|!'){
+            if(eleSyn==='|!'){
               pos[eleName]=i;
             }
           }
-          if(syn[eleName]=='&'){
-            if(eleSyn=='&!'){
+          if(syn[eleName]==='&'){
+            if(eleSyn==='&!'){
               let last=0;
               for(let j=0;j<lasts.length;j++){
                 if(lasts[j]>pos[eleName]){
                   last=lasts[j];
                 }
               }
-              Object.keys(obj).forEach(()=>{
-                if(pos[obj]<last){
-                  delete pos[obj];
-                  delete syn[obj];
+              Object.keys(pos).forEach((key)=>{
+                if(pos[key]<last){
+                  delete pos[key];
+                  delete syn[key];
                 }
               })
               syn[eleName]=eleSyn;
               pos[eleName]=i;
             }
-            if(eleSyn=='|!'){
+            if(eleSyn==='|!'){
               syn[eleName]=eleSyn;
               pos[eleName]=i;
             }
-            if(eleSyn=='&'){
+            if(eleSyn==='&'){
               pos[eleName]=i;
             }
-            if(eleSyn=='|'){
+            if(eleSyn==='|'){
               let last=0;
               for(let j=0;j<lasts.length;j++){
                 if(lasts[j]>pos[eleName]){
                   last=lasts[j];
                 }
               }
-              Object.keys(obj).forEach(()=>{
-                if(pos[obj]<last){
-                  delete pos[obj];
-                  delete syn[obj];
+              Object.keys(pos).forEach((key)=>{
+                if(pos[key]<last){
+                  delete pos[key];
+                  delete syn[key];
                 }
               })
               syn[eleName]=eleSyn;
               pos[eleName]=i;
             }
           }
-          if(syn[eleName]=='&!'){
-            if(eleSyn=='&'){
+          if(syn[eleName]==='&!'){
+            if(eleSyn==='&'){
               let last=0;
               for(let j=0;j<lasts.length;j++){
                 if(lasts[j]>pos[eleName]){
                   last=lasts[j];
                 }
               }
-              Object.keys(obj).forEach(()=>{
-                if(pos[obj]<last){
-                  delete pos[obj];
-                  delete syn[obj];
+              Object.keys(pos).forEach((key)=>{
+                if(pos[key]<last){
+                  delete pos[key];
+                  delete syn[key];
                 }
               })
               syn[eleName]=eleSyn;
               pos[eleName]=i;
             }
-            if(eleSyn=='|'){
+            if(eleSyn==='|'){
               syn[eleName]=eleSyn;
               pos[eleName]=i;
             }
-            if(eleSyn=='&!'){
+            if(eleSyn==='&!'){
               pos[eleName]=i;
             }
-            if(eleSyn=='|!'){
+            if(eleSyn==='|!'){
               let last=0;
               for(let j=0;j<lasts.length;j++){
                 if(lasts[j]>pos[eleName]){
                   last=lasts[j];
                 }
               }
-              Object.keys(obj).forEach(()=>{
-                if(pos[obj]<last){
-                  delete pos[obj];
-                  delete syn[obj];
+              Object.keys(pos).forEach((key)=>{
+                if(pos[key]<last){
+                  delete pos[key];
+                  delete syn[key];
                 }
               })
               syn[eleName]=eleSyn;
@@ -309,7 +396,7 @@ class App extends React.Component{
         pos[eleName]=i;
       }
     }  
-    workWithNo(syn,pos)
+    this.workWithNo(syn,pos)
   }
 
   input = e => {
