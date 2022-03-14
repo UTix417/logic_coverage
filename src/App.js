f8598f51333dc87eae1d1688d0ce0a8dac3514b6
lxf <lxf417@qq.com>
2022-03-14 22:53:13 +0800

    20220314

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends React.Component {
 			text: "",
 			loading: false,
 			solveType: 0,// 1条件判定 2条件组合 3MC/DC
+			tureStr:'',
 		}
 	}
 
@@ -99,11 +100,11 @@ class App extends React.Component {
 		let tureStr = str.substring(num, str.length - num)
 		for (let i = 0; i < tureStr.length; i++) {
 			if (tureStr[i] === '(') {
+				this.workWithHave(tureStr);
 				return;
-				this.makeElementSimpleWithNo(tureStr);
 			}
 		}
-		this.workWithHave(tureStr);
+		this.makeElementSimpleWithNo(tureStr);
 	}
 
 	workWithNo = (syn, pos) => {
@@ -138,7 +139,7 @@ class App extends React.Component {
 							fTo: '$F',
 						});
 						toTIndex.forEach((value) => {
-							map[value].tTo == map.size() - 1;
+							map[value].tTo = map.size() - 1;
 						})
 						toTIndex = map.size() - 1;
 						toFIndex.push(map.size() - 1);
@@ -181,9 +182,43 @@ class App extends React.Component {
 		})
 	}
 
-	workWithHave = (tureStr) => {
+	workWithHave = async (tureStr) => {
 		tureStr = await this.toFormat(tureStr);
-		console.log(tureStr);
+		this.setState({tureStr});
+		//console.log(tureStr,'???');
+		if(this.state.solveType==1){
+			let target={};
+			let elements=[];
+			let element='',elememtArea='';
+			let res=[];
+			for(let i=0;i<tureStr;i++){
+				if(tureStr[i]=='|'){
+					target[elememtArea]=[];
+					elements[element]=1;
+					element='';
+					elememtArea='';
+				}else if(tureStr[i]=='&'){
+					elements[element]=1;
+					element='';
+				}else{
+					elememtArea+=tureStr[i];
+					if(tureStr[i]!='!'){
+						element+=tureStr[i];
+					}
+				}
+			}
+			//可能需要化简
+			let tempRes=[];
+			for(let i=0;i<elements.length;i++){
+				tempRes.push(1);
+			}
+			res.push(tempRes);
+			tempRes=[];
+			for(let i=0;i<elements.length;i++){
+				tempRes.push(0);
+			}
+			res.push(tempRes);
+		}
 	}
 
 	toFormat = async (str) => {
@@ -204,15 +239,15 @@ class App extends React.Component {
 			}
 			if (nowNotFlag) {
 				if (str[i] == '&') {
-					str = str.subString(0, i) + '|' + str.subString(i + 1, str.length);
+					str = str.substring(0, i) + '|' + str.substring(i + 1, str.length);
 				} else if (str[i] == '|') {
-					str = str.subString(0, i) + '&' + str.subString(i + 1, str.length);
+					str = str.substring(0, i) + '&' + str.substring(i + 1, str.length);
 				} else if (str[i] == '!') {
 					let j = i + 1;
 					while (j < str.length && (str[j] != '|' || str[j] != '&')) {
 						j++;
 					}
-					str = str.subString(0, i) + str.subString(i + 1, str.length);
+					str = str.substring(0, i) + str.substring(i + 1, str.length);
 					i = j - 1;
 				} else if (str[i] == '(') {
 					let j = i + 1;
@@ -220,88 +255,136 @@ class App extends React.Component {
 					tempNum.push(i);
 					while (j < str.length && tempNum.length!=0) {
 						if(str[j]=='('){
-							temp.push(j);
+							tempNum.push(j);
 						}
 						if(str[j]==')'){
 							tempNum.pop();
 						}
 						j++;
 					}
-					str = str.subString(0, i) + '!' + str.subString(i, str.length);
+					str = str.substring(0, i) + '!' + str.substring(i, str.length);
 					i = j;
 				} else {//元素
 					let j = i + 1;
 					while (j < str.length && (str[j] != '|' || str[j] != '&')) {
 						j++;
 					}
-					str = str.subString(0, i) + '!' + str.subString(i, str.length);
+					str = str.substring(0, i) + '!' + str.substring(i, str.length);
 					i = j - 1;
 				}
 			}
 			if (str[i] == ')') {
 				num.pop();
 				if (num.length == 0) {
-					let res = await this.toFormat(str.subString(startIndex, i));
-					if(str[startIndex-1]=='!')
+					let res = await this.toFormat(str.substring(startIndex, i));
 					let tempStr = str;
 					str = '[' + res + ']';
 					if (startIndex > 0) {
-						str = tempStr.subString(0, startIndex - 1 - nowNotFlag) + str;
+						str = tempStr.substring(0, startIndex - 1 - nowNotFlag) + str;
 					}
 					i = str.length - 1;
 					if (i < tempStr.length - 1) {
-						str = str + tempStr.subString(i + 1, tempStr.length);
+						str = str + tempStr.substring(i + 1, tempStr.length);
 					}
 				}
-				nowNotFlag ^= 1;
+			}
+		}
+		let elementStar=0;
+		let inFlag=false;
+		//console.log(str,'递归结束');
+		for(let i=0;i<str.length;i++){
+			if(str[i]=='['){
+				inFlag=true;
+			}
+			if(str[i]==']'){
+				inFlag=false;
+			}
+			if((str[i]=='|'||str[i]=='&')&&!inFlag){
+				if(str[i-1]!=']'){
+					str=str.substring(0,elementStar)+'['+str.substring(elementStar,i)+']'+str.substring(i,str.length);
+					i+=2;
+				}
+				elementStar=i+1;
+			}
+			if(i==str.length-1&&!inFlag){
+				if(str[i]!=']'){
+					str=str.substring(0,elementStar)+'['+str.substring(elementStar,str.length)+']';
+				}
 			}
 		}
 		//括号内运算
 		let lastSyn = '';
 		let deleteStart = -1;
 		let andFlag = false;
+		// let emptyFlag = false;
+		//console.log(str,'括号内运算')
 		for (let i = 0; i < str.length; i++) {
 			if (str[i] == '|' || str[i] == '&') {
 				lastSyn = str[i];
 			}
 			if (str[i] == '[') {
 				deleteStart = i + 1;
-				if (lastSyn == '|' || lastSyn == '') {
+				if (lastSyn == '|') {
 					// nothing need to do
+				} else if(lastSyn == ''){
+					// emptyFlag = true;
 				} else {
 					andFlag = true;
 				}
 			}
 			if (str[i] == ']') {
-				if (!andFlag) {
-					str = str.subString(0, deleteStart - 1) + str.subString(deleteStart, i);
-				} else {
+				// if(emptyFlag){
+				// 	if(str[i+1]=='&'){
+				// 		andFlag=true;
+				// 	}		
+				// }
+				// if (!andFlag) {
+				// 	str = str.substring(0, deleteStart - 1) + str.substring(deleteStart, i)+str.substring(i+1,str.length);
+				// 	i-=2;
+				// }
+				if(andFlag) {
 					//deleteStart-2 是乘法的结束
-					let inElement = [], beforeElement = [];
+					let inElement = [];
+					let beforeElement = [];
 					let tempElement = '';
-					for (let j = 0; j < deleteStart - 1; j++) {
-						if (str[i] == '|') {
-							beforeElement.push(tempElement);
+					let isBefore=false;
+					for (let j = deleteStart - 2; j >=0 ; j--) {
+						if(str[j]==']'||!isBefore){
+							isBefore=true;
+							continue;
+						}
+						if(str[j]=='['){
+							beforeElement.push(tempElement.split('').reverse().join(''));
+							break;
+						}
+						if (str[j] == '|') {
+							beforeElement.push(tempElement.split('').reverse().join(''));
 							tempElement = '';
 						} else {
-							tempElement += str[i];
+							tempElement += str[j];
 						}
+						
 					}
 					tempElement = '';
 					for (let j = deleteStart; j < i; j++) {
-						if (str[i] == '|') {
+						if (str[j] == '|') {
 							inElement.push(tempElement);
 							tempElement = '';
 						} else {
-							tempElement += str[i];
+							tempElement += str[j];
+						}
+						if(j==i-1){
+							inElement.push(tempElement);
+							tempElement = '';
 						}
 					}
+					//console.log(inElement,beforeElement)
 					let resStr='';
 					for (let x = 0;x<beforeElement.length;x++){
 						for(let y=0;y<inElement.length;y++){
 							let newelement=beforeElement[x]+'&'+inElement[y];
 							let elementShowRecord={};
-							let elememt='';
+							let element='';
 							let elementStart=-1;
 							for(let j=0;j<newelement.length;j++){
 								if(newelement[j]=='&'||j==newelement.length-1){
@@ -309,17 +392,17 @@ class App extends React.Component {
 										elementShowRecord[element]=1;
 									}else{
 										let copynewelement=newelement;
-										newelement=copynewelement.subString(0,elementStart);
+										newelement=copynewelement.substring(0,elementStart);
 										if(j!=copynewelement.length-1){
-											newelement+=copynewelement.subString(j+1,copynewelement.length);
+											newelement+=copynewelement.substring(j+1,copynewelement.length);
 										}
 									}
 									if(resStr.length==0){
 										resStr+=newelement;
-									}else{
-										resStr+='|'+newelement;
+									}else if(newelement.length>0){
+										resStr=resStr+'|'+newelement;
 									}
-									elememt='';
+									element='';
 									elementStart=-1;
 								}else{
 									elementStart=i;
@@ -330,17 +413,19 @@ class App extends React.Component {
 					if(i==str.length-1){
 						str=resStr;
 					}else{
-						str=resStr+str.subString(i+1,str.length);
+						str=resStr+str.substring(i+1,str.length);
 					}
 					andFlag = false;
 				}
 				deleteStart = -1;
 			}
 		}
+		str=str.replaceAll('[','');
+		str=str.replaceAll(']','');
 		return str;
 	}
 
-	makeElementSimpleWithNo = (tureStr) => {
+	makeElementSimpleWithNo = (tureStr) => {//这里的消除逻辑有问题，要重新推导
 		let syn = {};
 		let pos = {};
 		for (let i = 0; i < tureStr.length;) {
@@ -561,8 +646,8 @@ class App extends React.Component {
 							loading={this.state.loading}
 						/>
 					</div>
+					
 				</div>
-
 			</div>
 		);
 	}
